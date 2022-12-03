9bd160ccda0ae3778b7703fbc311d5f1c5b38c94
Richard Bann <richard@bann.com>
2022-12-02 19:07:46 -0600

    MODEL TESTS

--- Minesweeper/test/model_test.cxx
+++ Minesweeper/test/model_test.cxx
@@ -3,228 +3,108 @@
 
 using namespace ge211;
 
-// These pass with the starter code and should continue
-// to pass.
-TEST_CASE("Passes with starter code")
+/// MINESWEEPER TESTS START HERE
+
+/// This tests if the game can be run completely. We declare a position set
+/// of bombs, and then eventually play a move at one of bombs. The game should
+/// realise that the player has died, as well as display the locations of all
+/// the bombs. (See line 44 and 45)
+TEST_CASE("set_game_complete")
 {
     Model m(8, 8);
-    CHECK(m.board() == Model::Rectangle {0, 0, 8, 8});
-    CHECK_FALSE(m.is_game_over());
-    CHECK(m.turn() == Player::dark);
-    CHECK(m.winner() == Player::neither);
-    CHECK(m[{0, 0}] == Player::neither);
-    CHECK(m.find_move({0, 0}) == nullptr);
-    CHECK_THROWS_AS(m.play_move({0, 0}), Client_logic_error);
-}
+    // get board, then get pset bombs
+    Position_set bombs = m.returnBoard().getPset("bombs_");
 
-// This fails with the starter code, but should pass.
-TEST_CASE("Fails with starter code")
-{
-    Model m(6);
-    CHECK(m.find_move({2, 2}));
-}
-
-// This fails with the starter code, but should pass.
-TEST_CASE("Throws with starter code")
-{
-    Model m(4);
-    m.play_move({1, 1});
-}
-
-// YOU NEED MANY MORE TESTS!
-
-
-///
-/// Example of how to set up and use Test_access
-///
-
-struct Test_access
-{
-    Model& model;
-
-    // Constructs a `Test_access` with a reference to the Model under test.
-    explicit Test_access(Model&);
-    // Sets the player at `posn` to `player`.
-    void set_player(Model::Position posn, Player player);
-    // Gives direct access to `model.next_moves_` so our tests can modify it:
-    Move_map& next_moves();
-};
-
-// Example of using Test_access to modify the model state to set up our test.
-TEST_CASE("Small Game")
-{
-    ///
-    /// SETUP
-    ///
-
-    Model model(4, 4);
-    Test_access access(model);
-
-    // Use Test_access to place pieces on the board without going through
-    // Model::play_move():
-    access.set_player({1, 1}, Player::dark);
-    access.set_player({2, 1}, Player::dark);
-    access.set_player({1, 2}, Player::dark);
-    access.set_player({2, 2}, Player::dark);
-
-    // Use Test_access to set `model.next_moves_` to allow the particular
-    // (illegal) move that we'd like to try:
-    access.next_moves()[{0, 0}] = Position_set {{0, 0}};
-
-    ///
-    /// TEST OPERATION
-    ///
-
-    // Now we can play the move, because model.next_moves_ says so:
-    model.play_move({0, 0});
-
-    ///
-    /// CHECKS
-    ///
-
-    // Check that the game is over and Dark won:
-    CHECK(model.turn() == Player::neither);
-    CHECK(model.winner() == Player::dark);
-
-    // Construct the set of board positions that should contain Player::dark:
-    Position_set darks {{0, 0}, {1, 1}, {2, 1}, {1, 2}, {2, 2}};
-
-    // Go through each position on the board, confirming that the positions in
-    // the `darks` set contain Player::dark and all other positions contain
-    // Player::neither.
-    for (auto posn : model.board()) {
-        Player expected = darks[posn] ? Player::dark : Player::neither;
-        CHECK(model[posn] == expected);
+    Posn<int> bomb(0,0);
+    int count = 0;
+    for (Posn<int> b : bombs)
+    {
+        count++;
+        if (count == 1)
+        {
+             bomb = b;
+        }
     }
+    m.play_move(bomb);
+
+    int c = 0;
+
+    Position_set seen = m.returnBoard().getPset("seen_");
+    for (Posn<int> s : seen)
+    {
+        for (Posn<int> b : bombs)
+        {
+            if (s == b)
+            {
+                c++;
+            }
+        }
+    }
+
+    CHECK(m.died_ == true);
+    CHECK(c == 10);
 }
 
-
-TEST_CASE("full board end game")
+/// This tests if the game begins properly by revealing one safe tile. We
+/// declare position sets of bomb tiles and seen tiles, and test if the tile
+/// we reveal is in the set of bombs or not. If it's not, then the tile is
+/// safe. See the CHECK_FALSE at line 70.
+TEST_CASE("find_safe_pos")
 {
-    ///
-    /// SETUP
-    ///
+    Model m(8,8);
+    Position_set seen = m.returnBoard().getPset("seen_");
+    Position_set bombs = m.returnBoard().getPset("bombs_");
 
+    bool check = false;
 
-    Model model(4, 4);
-    Test_access access(model);
-
-    // Use Test_access to place pieces on the board without going through
-    // Model::play_move():
-    access.set_player({0, 0}, Player::dark);
-    access.set_player({0, 1}, Player::dark);
-    access.set_player({0, 2}, Player::dark);
-    access.set_player({0, 3}, Player::dark);
-
-    access.set_player({1, 0}, Player::light);
-    access.set_player({1, 1}, Player::light);
-    access.set_player({1, 2}, Player::light);
-    access.set_player({1, 3}, Player::light);
-
-    access.set_player({2, 0}, Player::light);
-    access.set_player({2, 1}, Player::light);
-    access.set_player({2, 2}, Player::light);
-    access.set_player({2, 3}, Player::light);
-
-    access.set_player({3, 0}, Player::dark);
-    access.set_player({3, 1}, Player::dark);
-    access.set_player({3, 2}, Player::dark);
-    access.set_player({3, 3}, Player::dark);
-
-    // Use Test_access to set `model.next_moves_` to allow the particular
-    // (illegal) move that we'd like to try:
-    access.next_moves()[{0, 0}] = Position_set {{0, 0}};
-
-    ///
-    /// TEST OPERATION
-    ///
-
-    // Now we can play the move, because model.next_moves_
-    model.play_move({0, 0});
-
-    ///
-    /// CHECKS
-    ///
-
-    // Check that the game is over and Dark won:
-    CHECK(model.turn() == Player::neither);
-    CHECK(model.winner() == Player::light);
-    //
-    // // Construct the set of board positions that should contain Player::dark:
-    // Position_set darks {{0, 0}, {1, 1}, {2, 1}, {1, 2}, {2, 2}};
-    //
-    // // Go through each position on the board, confirming that the positions in
-    // // the `darks` set contain Player::dark and all other positions contain
-    // // Player::neither.
-    // for (auto posn : model.board()) {
-    //     Player expected = darks[posn] ? Player::dark : Player::neither;
-    //     CHECK(model[posn] == expected);
-    // }
+    for (Posn<int> b : bombs)
+    {
+        for (Posn<int> s : seen)
+        {
+            if (s == b)
+            {
+                check = true;
+            }
+        }
+    }
+    CHECK_FALSE(check);
 }
 
-
-TEST_CASE("playmove11")
+/// This tests if the game can play a move correctly. We have a set of seen
+/// positions, and then play a move somewhere on the board. The game should
+/// add that played position to the set of seen tiles (see line 89).
+TEST_CASE("play_move")
 {
-    ///
-    /// SETUP
-    ///
-
-
-    Model model(4, 4);
-    Test_access access(model);
-
-    // Use Test_access to place pieces on the board without going through
-    // Model::play_move():
-    access.set_player({0, 0}, Player::dark);
-    access.set_player({0, 1}, Player::dark);
-
-
-    // Use Test_access to set `model.next_moves_` to allow the particular
-    // (illegal) move that we'd like to try:
-    access.next_moves()[{0, 0}] = Position_set {{0, 0}};
-
-    ///
-    /// TEST OPERATION
-    ///
-
-    // Now we can play the move, because model.next_moves_
-    model.play_move({1, 1});
-
-    ///
-    /// CHECKS
-    ///
-    CHECK(model[{1,1}] == Player::dark);
-    // Check that the game is over and Dark won:
-    // CHECK(model.turn() == Player::neither);
-    // CHECK(model.winner() == Player::light);
-    //
-    // // Construct the set of board positions that should contain Player::dark:
-    // Position_set darks {{0, 0}, {1, 1}, {2, 1}, {1, 2}, {2, 2}};
-    //
-    // // Go through each position on the board, confirming that the positions in
-    // // the `darks` set contain Player::dark and all other positions contain
-    // // Player::neither.
-    // for (auto posn : model.board()) {
-    //     Player expected = darks[posn] ? Player::dark : Player::neither;
-    //     CHECK(model[posn] == expected);
-    // }
-}
-///
-/// Member function definitions for Test_access
-///
-
-Test_access::Test_access(Model& model)
-        : model(model)
-{ }
-
-void
-Test_access::set_player(Model::Position posn, Player player)
-{
-    model.board_[posn] = player;
+    Model m(8, 8);
+    m.play_move({1, 1});
+    Position_set seen = m.returnBoard().getPset("seen_");
+    bool check = false;
+    for (Posn<int> s : seen)
+    {
+        if (s == Posn<int>(1, 1))
+        {
+            check = true;
+        }
+    }
+    CHECK(check);
 }
 
-Move_map&
-Test_access::next_moves()
+/// This tests the game can generate 10 UNIQUE random positions of bombs.
+/// See line 109.
+TEST_CASE("random_positions_for_bombs")
 {
-    return model.next_moves_;
+    Model m(8,8);
+
+    Position_set bombs = m.returnBoard().getPset("bombs_");
+
+    int count = 0;
+
+    Position_set seen;
+
+    for (Posn<int> b : bombs)
+    {
+        seen[b] = true;
+        count++;
+    }
+    CHECK(seen.size() == 10);
 }
